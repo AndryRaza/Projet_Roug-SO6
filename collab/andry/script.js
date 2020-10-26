d29c6bf81aa727d4d57156ae44e40ebefcc03451
AndryRaza <70998539+AndryRaza@users.noreply.github.com>
2020-10-26 15:43:51 +0400

    Factorisation code JS

--- collab/andry/script.js
+++ collab/andry/script.js
@@ -1,99 +1,58 @@
 window.onload = function () {
 
-  window.addEventListener('scroll', function() {
-    
+  window.addEventListener('scroll', function () {
+
     if (window.scrollY >= 1500) {
       window.addEventListener('scroll', move_trajets);
-      window.addEventListener('scroll', move_vehicules);
-      window.addEventListener('scroll',move_employes);
-      window.addEventListener('scroll',move_pts);
+      window.addEventListener('scroll', chiffre(230, 1, "vehicules", anim_vehicules, 10));
+      window.addEventListener('scroll', chiffre(33, 1, "employes", anim_employes, 60));
+      window.addEventListener('scroll', chiffre(6, 1, "points_de_retrait", anim_pts, 350));
     }
   });
-    
 
-    var anim_trajets = false;
-    var anim_vehicules = false;
-    var anim_employes = false;
-    var anim_pts = false;
 
-    /*
-      function animate() {
-        
-         if (x > 170)
-         {
-           document.getElementById('cool').classList.add('tracking-in-expand-fwd')
-         }
-      }
-      */
-    function move_trajets() {
-      if (!anim_trajets) {
-        var elem = document.getElementById("trajets");
-        var i = 0;
-        var id = setInterval(frame, 10);
-        function frame() {
-          if (i == 80000) {
-            elem.innerHTML ="+ de "+  i;
-            anim_trajets = true;
-            clearInterval(id);
-          } else {
-            i = i + 500;
-            elem.innerHTML ="+ de "+ i;
-          }
+  var anim_trajets = false;
+  var anim_vehicules = false;
+  var anim_employes = false;
+  var anim_pts = false;
+
+  function move_trajets() {
+    if (!anim_trajets) {
+      var elem = document.getElementById("trajets");
+      var i = 0;
+      var id = setInterval(frame, 10);
+      function frame() {
+        if (i == 80000) {
+          elem.innerHTML = "+ de " + i;
+          anim_trajets = true;
+          clearInterval(id);
+        } else {
+          i = i + 500;
+          elem.innerHTML = "+ de " + i;
         }
       }
     }
-
-    function move_vehicules() {
-        if (!anim_vehicules) {
-          var elem = document.getElementById("vehicules");
-          var i = 0;
-          var id = setInterval(frame, 10);
-          function frame() {
-            if (i == 230) {
-              elem.innerHTML =i;
-              anim_vehicules = true;
-              clearInterval(id);
-            } else {
-              i = i + 1;
-              elem.innerHTML =i;
-            }
-          }
-        }
-      }
-
-      function move_employes() {
-        if (!anim_employes) {
-          var elem = document.getElementById("employes");
-          var i = 0;
-          var id = setInterval(frame, 60);
-          function frame() {
-            if (i == 33) {
-              elem.innerHTML =i;
-              anim_employes = true;
-              clearInterval(id);
-            } else {
-              i = i + 1;
-              elem.innerHTML =i;
-            }
-          }
-        }
-      }
-
-      function move_pts() {
-        if (!anim_pts) {
-          var elem = document.getElementById("points_de_retrait");
-          var i = 0;
-          var id = setInterval(frame, 350);
-          function frame() {
-            if (i == 6) {
-              elem.innerHTML =i;
-              anim_pts = true;
-              clearInterval(id);
-            } else {
-              i = i + 1;
-              elem.innerHTML =i;
-            }
-          }
-        }
-      }
   }
+
+
+
+  function chiffre(max, interval, id, bool_, time) {
+    if (!bool_) {
+      var elem = document.getElementById(id)
+      var i = 0;
+      var id = setInterval(frame, time);
+      function frame() {
+        if (i == max) {
+          elem.innerHTML = i;
+          bool_ = true;
+          clearInterval(id);
+        } else {
+          i = i + interval;
+          elem.innerHTML = i;
+        }
+      }
+    }
+  }
+
+
+}
